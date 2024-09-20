7b6abfad7998d2e668b5a29d88242604f00bf980
tiagolofi <jejeco75@gmail.com>
2024-09-20 00:15:15 -0300

    uso recursos

--- recursos.py
+++ recursos.py
@@ -20,7 +20,7 @@ def cria_dataframe(trigger, minr, maxr):
     return df
 
 
-st.title('análise de recursos')
+st.title('Estratégia Kubernetes x Uso de Recursos')
 
 col1, col2, col3, col4 = st.columns(4)
 
@@ -46,10 +46,10 @@ with col4_1:
     cpu_usage = st.number_input('cpuUsage', min_value=100, value=100, step = 50)
 
 with col4_2:
-    cpu_trigger = st.number_input('cpuTrigger', min_value=50, max_value=100, value=50, step=1)
+    memory_usage = st.number_input('memoryUsage', min_value=100, value=100, step = 50)
 
 with col4_3:
-    memory_usage = st.number_input('memoryUsage', min_value=100, value=100, step = 50)
+    cpu_trigger = st.number_input('cpuTrigger', min_value=50, max_value=100, value=50, step=1)
 
 with col4_4:
     memory_trigger = st.number_input('memoryTrigger', min_value=50, max_value=100, value=50, step=1)
@@ -58,11 +58,11 @@ col5_1, col5_2 = st.columns(2)
 
 with col5_1:
 
-    df1 = cria_dataframe(memory_trigger, min_replicas, max_replicas)
+    df1 = cria_dataframe(cpu_trigger, min_replicas, max_replicas)
     bola1 = int(round((cpu_usage / cpu_limit) * 100, 0))
-    z1 = int(df1[(df1[1] == bola1)][2])
+    z1 = int(df1[(df1[0] == bola1)][2])
 
-    fig1 = go.Figure(data=[go.Scatter3d(z=df1[2].values, x=df1[0].values, y=df1[1].values, mode='lines')])
+    fig1 = go.Figure(data=[go.Scatter3d(z=df1[2].values, x=df1[0].values, y=df1[1].values, mode='lines', name='estratégia CPU')])
 
     fig1.add_trace(go.Scatter3d(
         x=[bola1],
@@ -87,11 +87,11 @@ with col5_1:
 
 with col5_2:
 
-    df2 = cria_dataframe(cpu_trigger, min_replicas, max_replicas)
+    df2 = cria_dataframe(memory_trigger, min_replicas, max_replicas)
     bola2 = int(round((memory_usage / memory_limit) * 100, 0))
-    z2 = int(df2[(df2[0] == bola2)][2])
+    z2 = int(df2[(df2[1] == bola2)][2])
 
-    fig2 = go.Figure(data=[go.Scatter3d(z=df2[2].values, x=df2[0].values, y=df2[1].values, mode='lines')])
+    fig2 = go.Figure(data=[go.Scatter3d(z=df2[2].values, x=df2[0].values, y=df2[1].values, mode='lines', name='estratégia Memória')])
 
     fig2.add_trace(go.Scatter3d(
         x=[bola2],
